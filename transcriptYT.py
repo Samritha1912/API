e4aa5b2ca4f53bfdac6b536b49f6d02a7b4f6f42
Samritha R <105829950+Samritha1912@users.noreply.github.com>
2023-06-29 11:46:46 +0530

    Add files via upload
    A simple Python code that uses the YouTube API transcript to get the transcript of any video where the video ID is specified.

--- transcriptYT.py
+++ transcriptYT.py
@@ -5,5 +5,13 @@ from youtube_transcript_api import YouTubeTranscriptApi
 
 YouTubeTranscriptApi.get_transcript("Z6nkEZyS9nA")
 a =YouTubeTranscriptApi.get_transcript('Z6nkEZyS9nA')
-for i in a:
-    print(i['text'])
+message=""
+def trans():
+    for i in a:
+        #b=(i['text'])
+        print(i['text'])
+    
+
+trans()
+#print("HEllo")
+#print(a)
